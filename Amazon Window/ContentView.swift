af77ab571b13ea9588f72cf371b5661099d3dd17
SShaurya47 <107280326+SShaurya47@users.noreply.github.com>
2025-03-22 17:14:25 -0700

    Update ContentView.swift

--- Amazon Window/ContentView.swift
+++ Amazon Window/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hey, world!")
+            Text("Greetings, world!")
         }
         .padding()
     }
